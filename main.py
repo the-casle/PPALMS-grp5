022e1051b01c347972fe51192af47c450db88e04
the-casle <redeyedtreefrog28@gmail.com>
2022-11-28 13:19:30 -0600

    Using tab so selecting lines is easier

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
 import os
 import wx
 
+class Mp3Panel(wx.Panel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.row_obj_dict = {}
+
+        self.list_ctrl = wx.ListCtrl(
+            self,
+            style=wx.LC_REPORT | wx.BORDER_SUNKEN
+        )
+        self.list_ctrl.InsertColumn(0, 'Artist', width=10)
+        self.list_ctrl.InsertColumn(1, 'Lines', width=140)
+        main_sizer.Add(self.list_ctrl, 1, wx.ALL | wx.EXPAND, 5)
+        edit_button = wx.Button(self, label='Edit')
+        edit_button.Bind(wx.EVT_BUTTON, self.on_edit)
+        main_sizer.Add(edit_button, 0, wx.ALL | wx.CENTER, 5)
+        self.SetSizer(main_sizer)
+
+    def on_edit(self, event):
+        dialog = wx.FileDialog(self, "Open Source File",
+                               style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+
+        if dialog.ShowModal() == wx.ID_CANCEL:
+            return
+
+        path = dialog.GetPath()
+
+        if os.path.exists(path):
+            with open(path) as fobj:
+                i = 0
+                for line in fobj:
+                    self.list_ctrl.InsertItem(i, line)
+                    i += 1
+
+
+    def update_mp3_listing(self, folder_path):
+        print(folder_path)
+
 class MyPanel(wx.Panel):
 
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
 
-        self.my_text = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.my_text = wx.TextCtrl(self, style=wx.TE_MULTILINE | wx.TE_READONLY | wx.HSCROLL)
         btn = wx.Button(self, label='Open Text File')
         btn.Bind(wx.EVT_BUTTON, self.onOpen)
 
@@ -34,9 +72,9 @@ class MyPanel(wx.Panel):
 class MyFrame(wx.Frame):
 
     def __init__(self):
-        wx.Frame.__init__(self, None, title='Text File Reader')
+        wx.Frame.__init__(self, None, title='Annotation')
 
-        panel = MyPanel(self)
+        panel = Mp3Panel(self)
 
         self.Show()
 
