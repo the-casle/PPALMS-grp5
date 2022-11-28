81018f9f92fd9f330ca5746f338b7b1e87325a1d
the-casle <redeyedtreefrog28@gmail.com>
2022-11-27 21:24:54 -0600

    File browser

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+import wx
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class MyFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None, title='Hello World')
+        panel = wx.Panel(self)
+        my_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.text_ctrl = wx.TextCtrl(panel)
+        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
+        my_btn = wx.Button(panel, label='Browse Files')
+        my_btn.Bind(
+            event=wx.EVT_BUTTON,
+            handler=self.on_open_folder,
+        )
+        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)
+        panel.SetSizer(my_sizer)
+        self.Show()
 
+    def on_press(self, event):
+        value = self.text_ctrl.GetValue()
+        if not value:
+            print("You didn't enter anything!")
+        else:
+            print(f'You typed: "{value}"')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    def on_open_folder(self, event):
+        with wx.FileDialog(self, "Open Source file",
+                           style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
 
+            if fileDialog.ShowModal() == wx.ID_CANCEL:
+                return  # the user changed their mind
 
-# Press the green button in the gutter to run the script.
+            # Proceed loading the file chosen by the user
+            pathname = fileDialog.GetPath()
+            try:
+                with open(pathname, 'r') as file:
+                    self.doLoadDataOrWhatever(file)
+            except IOError:
+                wx.LogError("Cannot open file '%s'." % newfile)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = wx.App()
+    frame = MyFrame()
+    app.MainLoop()
